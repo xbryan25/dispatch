5c32303eef5796543cc59acb523ef3e82ab11e30
Bryan Agan <bryanagan25@gmail.com>
2026-01-15 20:04:29 +0800

    feat(frontend, auth): handle user creation using Supabase Auth
    Though not finalized yet

---
+++ frontend/lib/api.ts
@@ -0,0 +1,12 @@
+export async function api<T>(url: string, options?: RequestInit): Promise<T> {
+  const res = await fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    ...options,
+  });
+
+  if (!res.ok) {
+    throw new Error('API error');
+  }
+
+  return res.json();
+}
