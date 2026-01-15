1afa2c9815eeb98471fc64529a920c13fc52ec52
Bryan Agan <bryanagan25@gmail.com>
2026-01-15 20:28:32 +0800

    feat(frontend): make sure password and confirm password is identical in register page

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,8 +1,6 @@
 import { supabase } from '@/lib/supabase/client';
 
 export async function register(email: string, password: string, displayName: string) {
-  console.log({ email, password });
-
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
