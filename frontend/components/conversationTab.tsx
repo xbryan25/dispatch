88a3ab2a4f5b896fe03da50977e9217413a00a52
Bryan Agan <bryanagan25@gmail.com>
2026-01-25 20:26:53 +0800

    ui(frontend): add text truncation to ConversationTab component when ConversationDetails component is expanded

--- frontend/components/conversationTab.tsx
+++ frontend/components/conversationTab.tsx
@@ -1,26 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-
-import { Button } from './ui/button';
-
-import { useRouter, usePathname } from 'next/navigation';
-
-import ThemeToggleButton from './themeToggleButton';
-
-import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group';
-
-import { Search } from 'lucide-react';
 import { Icon } from '@iconify/react';
 import Image from 'next/image';
 
 export default function ConversationTab() {
-  const router = useRouter();
-
-  const pathname = usePathname();
-
   return (
-    <div className="flex items-center justify-center gap-2 bg-white dark:bg-stone-900 hover:bg-stone-200 dark:hover:bg-stone-700 rounded-md p-2 w-full cursor-pointer py-2">
+    <div className="flex items-center justify-center gap-2 bg-white dark:bg-stone-900 hover:bg-stone-200 dark:hover:bg-stone-700 rounded-md p-2 w-full cursor-pointer min-w-0 ">
       <div className="relative w-12 h-12 shrink-0 overflow-hidden rounded-full ">
         <Image
           src="/testdp.jpg"
@@ -31,14 +16,16 @@ export default function ConversationTab() {
         />
       </div>
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <div className="flex justify-between">
           <h3 className="font-semibold">Bryan Agan</h3>
-          <p className="">1:25 PM</p>
+          <p className="shrink-0">1:25 PM</p>
         </div>
-        <div className="flex justify-between items-center">
-          <p>You: Yo? What did you say to me? {"Don't"}...</p>
-          <Icon icon="ri:check-double-fill" className="size-5" />
+        <div className="flex justify-between items-center gap-4">
+          <p className="truncate ">
+            You: Yo? What did you say to me? {"Don't"}...aaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa
+          </p>
+          <Icon icon="ri:check-double-fill" className="size-5 shrink-0" />
         </div>
       </div>
     </div>
