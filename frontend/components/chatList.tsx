916ae1139fa61755cc152d7fda0e48f42022717e
Bryan Agan <bryanagan25@gmail.com>
2026-01-25 20:26:53 +0800

    ui(frontend): add text truncation to ConversationTab component when ConversationDetails component is expanded

--- frontend/components/chatList.tsx
+++ frontend/components/chatList.tsx
@@ -4,7 +4,7 @@ import ConversationTab from './conversationTab';
 
 export default function ChatList() {
   return (
-    <div className="flex-1 flex flex-col w-full gap-1 overflow-y-auto h-0 pr-3">
+    <div className="flex-1 flex flex-col w-full gap-1 overflow-y-auto h-0 pr-3 min-w-0">
       <ConversationTab />
       <ConversationTab />
       <ConversationTab />
