799e07c7357186d1ed08e054a54ca331e4928aec
Bryan Agan <bryanagan25@gmail.com>
2026-02-09 12:39:49 +0800

    fix(chat): fix new conversation not clearing old conversation messages when switching conversation tabs

--- frontend/components/chatList.tsx
+++ frontend/components/chatList.tsx
@@ -5,14 +5,11 @@ import ConversationTab from './conversationTab';
 import { useConversationTabs } from '@/hooks/useConversationTabs';
 
 import { useChat } from '@/hooks/useChat';
-import { useChatStore } from '@/store/useChatStore';
 
 export default function ChatList() {
   const { conversationSnippets } = useConversationTabs();
 
-  const selectedConversationId = useChatStore((state) => state.activeConversationId);
-
-  useChat(selectedConversationId);
+  useChat();
 
   return (
     <div className="flex-1 flex flex-col w-full gap-1 overflow-y-auto h-0 pr-3 min-w-0">
