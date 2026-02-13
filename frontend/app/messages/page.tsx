a3a3fb9b74a3d94f0109ceb4d3734f29501cc49c
Bryan Agan <bryanagan25@gmail.com>
2026-02-13 20:50:59 +0800

    ui(conversation_sidebar): show custom text if user has no conversations yet

--- frontend/app/messages/page.tsx
+++ frontend/app/messages/page.tsx
@@ -3,14 +3,26 @@
 import ChatSidebar from '@/components/chatSidebar';
 import ConversationArea from '@/components/conversationArea';
 import ConversationDetails from '@/components/conversationDetails';
-import { useState } from 'react';
+import { useChatStore } from '@/store/useChatStore';
+import { useEffect, useState } from 'react';
 
 export default function MessagesPage() {
   const [showConversationDetails, setShowConversationDetails] = useState<boolean>(false);
 
+  const { activeConversationId } = useChatStore();
+
+  useEffect(() => {
+    console.log(`current conversation id: ${activeConversationId}`);
+  }, [activeConversationId]);
+
   return (
     <div className="flex h-screen items-stretch justify-center gap-6 overflow-hidden bg-zinc-200 dark:bg-stone-800 font-sans  p-4">
       <ChatSidebar />
+
+      {/* {activeConversationId && (
+        <ConversationArea onToggle={() => setShowConversationDetails(!showConversationDetails)} />
+      )} */}
+
       <ConversationArea onToggle={() => setShowConversationDetails(!showConversationDetails)} />
 
       {showConversationDetails && <ConversationDetails />}
--- frontend/components/chatList.tsx
+++ frontend/components/chatList.tsx
@@ -11,16 +11,26 @@ export default function ChatList() {
 
   useChat();
 
-  return (
-    <div className="flex-1 flex flex-col w-full gap-1 overflow-y-auto h-0 pr-3 min-w-0">
-      {conversationSnippets.map((conversationSnippet) => {
-        return (
-          <ConversationTab
-            key={conversationSnippet.conversationId}
-            conversationSnippet={conversationSnippet}
-          />
-        );
-      })}
-    </div>
-  );
+  if (conversationSnippets.length == 0) {
+    return (
+      <div className="flex-1 flex w-full gap-1 items-center justify-center">
+        <p className="font-medium text-center text-xl">
+          Start messaging people so that they show up here!
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex-1 flex flex-col w-full gap-1 overflow-y-auto h-0 pr-3 min-w-0">
+        {conversationSnippets.map((conversationSnippet) => {
+          return (
+            <ConversationTab
+              key={conversationSnippet.conversationId}
+              conversationSnippet={conversationSnippet}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
