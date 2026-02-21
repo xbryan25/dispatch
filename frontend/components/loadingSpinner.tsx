cc492ef4c4787d97218df7a4a1483c3a71f1078c
Bryan Agan <bryanagan25@gmail.com>
2026-02-21 20:25:26 +0800

    feat(user_settings): add loading spinner when retrieving old user details

--- frontend/components/loadingSpinner.tsx
+++ frontend/components/loadingSpinner.tsx
@@ -1,9 +1,10 @@
-'use client';
+import { Spinner } from '@/components/ui/spinner';
 
 export default function LoadingSpinner() {
   return (
     <div className="flex flex-col gap-1 justify-center items-center">
-      <div className="h-6 w-6 animate-spin rounded-full border-3 border-gray-200 border-t-blue-600" />
+      <Spinner className="size-7" />
+
       <p className="text-md font-medium">Loading...</p>
     </div>
   );
