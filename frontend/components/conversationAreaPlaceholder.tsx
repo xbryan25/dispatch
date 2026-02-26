71b4e34168182b4d709e6621cdd247e77731def0
Bryan Agan <bryanagan25@gmail.com>
2026-02-26 11:53:09 +0800

    ui(chat): only show loading spinner on conversation area when switching between conversations

--- frontend/components/conversationAreaPlaceholder.tsx
+++ frontend/components/conversationAreaPlaceholder.tsx
@@ -1,23 +1,20 @@
 import { useSidebarStore } from '@/store/useSidebarStore';
-import LoadingSpinner from './loadingSpinner';
 
 export default function ConversationAreaPlaceholder() {
-  const { conversationSnippets, isLoading } = useSidebarStore();
+  const hasChats = useSidebarStore((state) => state.conversationSnippets.length > 0);
 
   return (
-    <div className="flex-3 flex justify-center items-center gap-2 bg-white dark:bg-stone-900 rounded-xl">
-      {isLoading && (
-        <div className="flex-1 flex w-full gap-1 items-center justify-center">
-          <LoadingSpinner />
-        </div>
-      )}
+    <div className="flex-3 flex flex-col justify-center items-center gap-2 bg-white dark:bg-stone-900 rounded-xl">
+      <h1 className="font-semibold text-3xl tracking-tight">
+        {hasChats
+          ? 'Select a conversation and jump back in!'
+          : 'Ready to break the ice? Start your first chat.'}
+      </h1>
 
-      {!isLoading && (
-        <h1 className="font-semibold text-3xl">
-          {conversationSnippets.length == 0
-            ? 'Ready to break the ice? Start your first chat.'
-            : 'Select a conversation and jump back in!'}
-        </h1>
+      {!hasChats && (
+        <p className="text-muted-foreground">
+          {"You don't have any messages yet. Search for a friend to begin."}
+        </p>
       )}
     </div>
   );
