b939fe204e2aac32cb93c56dc9efe65dcd08376d
Bryan Agan <bryanagan25@gmail.com>
2026-02-27 09:48:48 +0800

    feat(friends): create userCard that shows basic information of other users
    - Also has buttons that redirect to /messages page and opens unfriendDialog

---
+++ frontend/components/unfriendDialog.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button } from './ui/button';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+import { Icon } from '@iconify/react';
+
+export default function UnfriendDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size="icon" className="h-9 w-9 shrink-0 cursor-pointer">
+          <Icon icon="material-symbols:person-remove" width="20" height="20" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure you want to unfriend .username.?</DialogTitle>
+          <DialogDescription>
+            Your conversation with .username. will change to view-only.
+          </DialogDescription>
+          <div className="flex w-full gap-2 pt-2">
+            <Button className="flex-1 cursor-pointer text-md">Confirm unfriend</Button>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
