c83a0792f7363553d3f4686f936af7eee94f722e
Bryan Agan <bryanagan25@gmail.com>
2026-03-22 20:01:49 +0800

    refactor(chat): move message input to its own component

---
+++ frontend/components/messageInput.tsx
@@ -0,0 +1,78 @@
+import { Button } from './ui/button';
+
+import { InputGroup, InputGroupTextarea } from '@/components/ui/input-group';
+
+import { useState } from 'react';
+
+import { useSendMessage } from '@/hooks/useChat';
+import { useChatStore } from '@/store/useChatStore';
+import { OtherParticipantDetails } from '@/types/chat';
+
+interface MessageInputProps {
+  otherParticipantFriendshipStatus: string | null;
+  otherParticipantDetails: OtherParticipantDetails | null;
+}
+
+export default function MessageInput({
+  otherParticipantFriendshipStatus,
+  otherParticipantDetails,
+}: MessageInputProps) {
+  const [newMessage, setNewMessage] = useState('');
+
+  const { send, error: sendError } = useSendMessage();
+  const addSendingMessage = useChatStore((state) => state.addSendingMessage);
+  const removeSendingMessage = useChatStore((state) => state.removeSendingMessage);
+
+  const addFailedMessage = useChatStore((state) => state.addFailedMessage);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      if (newMessage.trim() !== '') sendNewMessage();
+    }
+  };
+
+  const sendNewMessage = async () => {
+    const tempMessageId: string = crypto.randomUUID();
+
+    addSendingMessage({
+      tempMessageId,
+      content: newMessage.trim(),
+      createdAt: new Date().toISOString(),
+      status: 'sending',
+    });
+
+    setNewMessage('');
+    await send(newMessage.trim(), tempMessageId);
+
+    if (sendError !== null) {
+      addFailedMessage({
+        tempMessageId,
+        content: newMessage.trim(),
+        createdAt: new Date().toISOString(),
+        status: 'sending',
+      });
+
+      removeSendingMessage(tempMessageId);
+    }
+  };
+
+  return (
+    <div className="flex gap-2 px-2 my-2 items-end">
+      <InputGroup className="flex-1 shrink-0 min-h-0!  ">
+        <InputGroupTextarea
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
+          className="min-h-4! py-2!"
+          placeholder={
+            otherParticipantFriendshipStatus !== 'accepted'
+              ? `You aren't friends with ${otherParticipantDetails?.username} anymore. This conversation is read-only.`
+              : 'Type your message...'
+          }
+        />
+      </InputGroup>
+      <Button onClick={sendNewMessage}>Send</Button>
+    </div>
+  );
+}
