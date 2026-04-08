be7cc23de5038687b612547eb1aba76c3746faf5
Bryan Agan <bryanagan25@gmail.com>
2026-04-08 21:09:03 +0800

    feat(notifications): mark unread notification as read when opening the dialog

--- frontend/components/notificationDetailsDialog.tsx
+++ frontend/components/notificationDetailsDialog.tsx
@@ -10,21 +10,50 @@ import {
 } from '@/components/ui/dialog';
 
 import { Separator } from '@/components/ui/separator';
+import { useNotificationsStore } from '@/store/useNotificationsStore';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface NotificationDetailsDialogProps {
+  notificationId: string;
   content: string;
   type: string;
+  toShowInCell: 'content' | 'type';
   dateStr: string;
 }
 
 export default function NotificationDetailsDialog({
+  notificationId,
   content,
   type,
+  toShowInCell,
   dateStr,
 }: NotificationDetailsDialogProps) {
+  const updateNotificationsReadStatus = useNotificationsStore(
+    (state) => state.updateNotificationsReadStatus
+  );
+
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toTitleCase = (str: string) =>
+    str
+      .split('_')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+
+  const wasOpened = useRef(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      wasOpened.current = true;
+    }
+
+    // TODO: Table should be updated when opening dialog, not when closing it
+    if (!isOpen && wasOpened.current) {
+      updateNotificationsReadStatus([notificationId], 'read');
+      wasOpened.current = false;
+    }
+  }, [isOpen]);
 
   return (
     <Dialog
@@ -34,12 +63,12 @@ export default function NotificationDetailsDialog({
       }}
     >
       <DialogTrigger asChild className="cursor-pointer">
-        <p>{content}</p>
+        <p>{toShowInCell === 'type' ? toTitleCase(type) : content}</p>
       </DialogTrigger>
 
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>{type}</DialogTitle>
+          <DialogTitle>{toTitleCase(type)}</DialogTitle>
         </DialogHeader>
         <Separator />
         <div className="flex w-full gap-2 pt-2">{content}</div>
