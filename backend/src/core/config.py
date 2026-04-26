6990a7d634024e7c20bf097d9b62816428c8830e
Bryan Agan <bryanagan25@gmail.com>
2026-04-26 22:35:19 +0800

    fix(cors): configure CORS Middleware for smooth communication between Vercel and Railway

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -14,6 +14,8 @@ class Settings(BaseSettings):
 
     REDIS_URL: str
 
+    FRONTEND_URL: str
+
     model_config = SettingsConfigDict(
         env_file="../.env", env_file_encoding="utf-8", extra="ignore"
     )
